68dee731f001e2aa5434def796aa2f8126053d23
Dharil33 <dharilpatel61857@gmail.com>
2023-03-20 19:08:21 +0530

    final commit by dharil

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify
 import pickle
 import pandas as pd
+import numpy as np
+# from src.exception import CustomException
+# import sys
+import joblib
 
 # initialize the flask app
 app = Flask(__name__, template_folder="templates")
-model = pickle.load(open("Fraud_detection.pkl", "rb"))
+model = pickle.load(open("final_model_prediction.pkl", "rb"))
+# scalar = pickle.load(open("StandardScaler.pkl","rb"))
+scalar = joblib.load("StandardScaler.pkl")
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,33 +18,26 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.fit_transform(np.array(list(data.values())).reshape(1,-1))
+    output=model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
 # Now predict function
 @app.route('/predict', methods=['POST'])
 def predict():
     # For rendering results on the HTML page
-    step = request.form['step']
-    type = request.form['type']
-    amount = request.form['amount']
-    oldbalanceOrg = request.form['oldbalanceOrg']
-    newbalanceOrig = request.form['newbalanceOrig']
-    oldbalanceDest = request.form['oldbalanceDest']
-    newbalanceDest = request.form['newbalanceDest']
-
-    step = int(step)
-    type = int(type)
-    amount = int(amount)
-    oldbalanceOrg = int(oldbalanceOrg)
-    newbalanceOrig = int(newbalanceOrig)
-    oldbalanceDest = float(oldbalanceDest)
-    newbalanceDest = float(newbalanceDest)
-
-    row_df = pd.DataFrame([pd.Series([step,type, amount, oldbalanceOrg, newbalanceOrig, oldbalanceDest, newbalanceDest])])
-    print(row_df)
-    prediction = model.predict_proba(row_df)
-    output = '{0:.{1}f}'.format(prediction[0][1], 2)
-    output = str(float(output) * 100) + '%'
-    return render_template('index.html', pred=f'Probability of having fraud is {output}')
-
+    # step = request.form['step']
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.fit_transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template("index.html",prediction_text=f"Probability of having fraud is {output}")
 
 if __name__ == '__main__':
     app.run(debug=True)
