1984fbde801544a9e9ee84c18a6a5d13ca41e894
Dharil33 <dharilpatel61857@gmail.com>
2023-03-20 22:06:39 +0530

    app.py dharil

--- app.py
+++ app.py
@@ -31,13 +31,15 @@ def predict_api():
 # Now predict function
 @app.route('/predict', methods=['POST'])
 def predict():
-    # For rendering results on the HTML page
-    # step = request.form['step']
     data=[float(x) for x in request.form.values()]
     final_input=scalar.fit_transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=model.predict(final_input)[0]
-    return render_template("index.html",prediction_text=f"Probability of having fraud is {output}")
+    if output == 0:
+        result = "Current Transaction is not fraud!!"
+    else:
+        result = "Current Transaction is fraud!!"
+    return render_template("index.html",prediction_text=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
